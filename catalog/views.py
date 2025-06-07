a478ff3674bf111c74d9e7da5f228aac9a0f3446
egor <scudf24@gmail.com>
2025-06-07 13:05:44 +0300

    completed fourth task (added admin, added and configured product and category models)

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
     return render(request, "catalog/home.html")
@@ -13,4 +14,4 @@ def contacts(request):
         message = request.POST.get("message")
 
         return HttpResponse(f"Спасибо, {name}! Сообщение получено.")
-    return render(request,"catalog/contacts.html")
+    return render(request, "catalog/contacts.html")
