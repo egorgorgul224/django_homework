d9c1285f8430d4779285a2b6c0be12fb245b4012
egor <scudf24@gmail.com>
2025-06-15 13:09:06 +0300

    completed 1 and 2 tasks (added pages with info for each product and all products on the main page)

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from catalog.models import Category, Product
 
 
 # Create your views here.
-def home(request):
-    return render(request, "catalog/home.html")
-
-
 def contacts(request):
     if request.method == "POST":
         name = request.POST.get("name")
@@ -15,3 +12,17 @@ def contacts(request):
 
         return HttpResponse(f"Спасибо, {name}! Сообщение получено.")
     return render(request, "catalog/contacts.html")
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {
+        "product": product,
+    }
+    return render(request, "catalog/product_detail.html", context=context)
+
+
+def product_list_home(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'catalog/home.html', context)
